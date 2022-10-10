c39d6d578b1481a913eb3e79e2187f18b3e6428f
tgibbons-css <tgibbons@css.edu>
2022-10-10 12:15:20 -0500

    Add project files.

---
+++ InsuranceSystem/InsuranceInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    interface InsuranceInterface
+    {
+        // verifies that the patient is currently covered by this policy
+        bool IsCovered(string patientName, string policyNumber);
+
+        decimal CoverageAmount(int ProcedureID, decimal ProcedureCost);
+
+        string getPatientName();
+    }
+}
---
+++ InsuranceSystem/OutNetworkAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    public class OutNetworkAdapter : InsuranceInterface
+    {
+        OutNetworkPatient patient;
+
+        public OutNetworkAdapter(string newName, int newPolicyNumber)
+        {
+            patient = new OutNetworkPatient(newName, newPolicyNumber);
+        }
+
+        public decimal CoverageAmount(int ProcedureID, decimal ProcedureCost)
+        {
+            decimal percentCovered = patient.CoveragePercent(ProcedureCost);
+            return ProcedureCost * percentCovered;
+        }
+
+        public string getPatientName()
+        {
+            return patient.getPatientName();
+        }
+
+        public bool IsCovered(string patientName, string policyNumber)
+        {
+            int intPolicyNumber = int.Parse(policyNumber);
+            string strCovered = patient.IsCovered(patientName, intPolicyNumber);
+
+            if (strCovered == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
