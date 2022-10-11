19ae2ad95271c496dc2625db0b604cf7bd51ac23
Tom Gibbons <tgibbons@css.edu>
2022-10-10 20:28:29 -0500

    Converted patientRepo into a dictionary index by policy number

--- InsuranceSystem/PatientRepository.cs
+++ InsuranceSystem/PatientRepository.cs
@@ -7,23 +7,24 @@ namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
 {
     public class PatientRepository
     {
-        List<InsuranceInterface> patients = new List<InsuranceInterface>();
+        //List<InsuranceInterface> patients = new List<InsuranceInterface>();
+        Dictionary<string, InsuranceInterface> patients = new Dictionary<string, InsuranceInterface>();
 
         public PatientRepository()
         {
-            patients.Add(new InNetworkPatient("Tom Gibbons", "A111B222", PolicyLevels.Gold));
-            patients.Add(new InNetworkPatient("Jen Rosato", "A222C444", PolicyLevels.Platnum));
-            patients.Add(new InNetworkPatient("Dave Vosen", "D333B111", PolicyLevels.Silver));
+            patients.Add("A111B222", new InNetworkPatient("Tom Gibbons", "A111B222", PolicyLevels.Gold));
+            patients.Add("A222C444", new InNetworkPatient("Jen Rosato", "A222C444", PolicyLevels.Platnum));
+            patients.Add("D333B111", new InNetworkPatient("Dave Vosen", "D333B111", PolicyLevels.Silver));
         }
 
-        public List<InsuranceInterface> getAllPatients()
+        public IEnumerable<InsuranceInterface> getAllPatients()
         {
-            return patients;
+            return patients.Values;
         }
 
         public InsuranceInterface getPatientByPolicy(string PolicyNumber) {
             // TODO --- actually return the patient with this policy number
-            return patients[0];
+            return patients[PolicyNumber];
         }
     }
 }
